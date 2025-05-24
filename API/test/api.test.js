8f2c8d474161b9351ad67941355e1fbbc621e1fd
Qwinton Knocklein <u21669849@tuks.co.za>
2025-05-24 17:56:51 +0200

    Added a test for default route

--- API/test/api.test.js
+++ API/test/api.test.js
@@ -7,6 +7,10 @@ describe('API Endpoints', function () {
     await connectToDb();
   });
 
-  
+  it('GET / should return a message and collections', async function () {
+    const res = await request(app).get('/');
+    expect(res.status).to.equal(200);
+    expect(res.body).to.have.property('message');
+  });
 
 });
