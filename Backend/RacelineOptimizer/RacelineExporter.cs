cafee4424a1bc4cf92c2a5d19103b1dd2753a0a1
Sean van der Merwe <seanvdmerwe2505@gmail.com>
2025-06-02 10:49:00 +0200

    Added function to write racing line to a binary file.
    This uses the binary writer function to place all the points of the track in a formatted binary file, so it can be used later.

--- Backend/RacelineOptimizer/RacelineExporter.cs
+++ Backend/RacelineOptimizer/RacelineExporter.cs
@@ -6,7 +6,16 @@ namespace RacelineOptimizer
 {
     public static class RacelineExporter
     {
-        
+        public static void SaveToBinary(string filePath, List<Vector2> inner, List<Vector2> outer, List<Vector2> raceline)
+        {
+            using (var fs = new FileStream(filePath, FileMode.Create, FileAccess.Write))
+            using (var bw = new BinaryWriter(fs))
+            {
+                WritePoints(bw, outer);     // First: OuterBoundary
+                WritePoints(bw, inner);     // Second: InnerBoundary
+                WritePoints(bw, raceline);  // Third: Raceline
+            }
+        }
 
         private static void WritePoints(BinaryWriter bw, List<Vector2> points)
         {
