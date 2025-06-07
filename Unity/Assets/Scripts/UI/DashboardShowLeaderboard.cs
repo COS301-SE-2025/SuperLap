c081ac817e40626b94c6043c23b9e72ead37ba4d
u04576617 <u04576617@tuks.co.za>
2025-06-07 17:35:24 +0200

    Fix conflict issues

--- Unity/Assets/Scripts/UI/DashboardShowLeaderboard.cs
+++ Unity/Assets/Scripts/UI/DashboardShowLeaderboard.cs
@@ -6,6 +6,8 @@ public class DashboardShowLeaderboard : MonoBehaviour
 {
   //Panel for leaderboard
   public GameObject leaderboard;
+  private RectTransform rectTransform;
+  private float leaderboardWidth;
 
   public TMP_Text leaderboardButtonText;
 
@@ -17,24 +19,27 @@ public class DashboardShowLeaderboard : MonoBehaviour
 
   public void Start()
   {
+    rectTransform = leaderboard.GetComponent<RectTransform>();
+    leaderboardWidth = rectTransform.rect.width;
+    Debug.Log("Leaderboard Width: " + leaderboardWidth);
     showLeaderboard = false;
-    newPosition = new Vector3(106, leaderboard.transform.position.y, leaderboard.transform.position.z);
-    leaderboard.transform.position = new Vector3(106, leaderboard.transform.position.y, leaderboard.transform.position.z);
+    newPosition = new Vector3(-360 - leaderboardWidth, rectTransform.anchoredPosition.y, rectTransform.transform.position.z);
+    rectTransform.anchoredPosition = newPosition;
   }
 
   void Update()
   {
-    if(leaderboard.transform.position.x != newPosition.x)
+    if(rectTransform.anchoredPosition.x != newPosition.x)
     {
-      leaderboard.transform.position = Vector3.MoveTowards(leaderboard.transform.position, newPosition, transitionSpeed * Time.deltaTime);
+      rectTransform.anchoredPosition = Vector3.MoveTowards(rectTransform.anchoredPosition, newPosition, transitionSpeed * Time.deltaTime);
     }
   }
 
   public void HideLeaderboard()
   {
     showLeaderboard = false;
-    newPosition = new Vector3(106, leaderboard.transform.position.y, leaderboard.transform.position.z);
-    leaderboard.transform.position = new Vector3(106, leaderboard.transform.position.y, leaderboard.transform.position.z);
+    newPosition = new Vector3(-360 - leaderboardWidth, rectTransform.anchoredPosition.y, 0);
+    rectTransform.anchoredPosition = new Vector3(-360, rectTransform.anchoredPosition.y, 0);
     leaderboardButtonText.text = ">";
   }
 
@@ -43,12 +48,12 @@ public class DashboardShowLeaderboard : MonoBehaviour
     showLeaderboard = !showLeaderboard;
     if(showLeaderboard)
     {
-      newPosition = new Vector3(460, leaderboard.transform.position.y, leaderboard.transform.position.z);
+      newPosition = new Vector3(-360, rectTransform.anchoredPosition.y, 0);
       leaderboardButtonText.text = "<";
     }
     else
     {
-      newPosition = new Vector3(106, leaderboard.transform.position.y, leaderboard.transform.position.z);
+      newPosition = new Vector3(-360-leaderboardWidth, rectTransform.anchoredPosition.y, 0);
       leaderboardButtonText.text = ">";
     }
   }
