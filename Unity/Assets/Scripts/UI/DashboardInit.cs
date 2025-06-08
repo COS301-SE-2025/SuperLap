0417d86785d7deeeab7738b2276753ade670ccd1
MilanThenerd <milankruger4@gmail.com>
2025-06-08 13:30:29 +0200

    Fixed Dashboard Init

--- Unity/Assets/Scripts/UI/DashboardInit.cs
+++ Unity/Assets/Scripts/UI/DashboardInit.cs
@@ -4,9 +4,11 @@ public class DashboardInit : MonoBehaviour
 {
   public GameObject dashboard;
   public GameObject importTrack;
+  public GameObject analysis;
   void Awake()
   {
     dashboard.SetActive(true);
     importTrack.SetActive(false);
+    analysis.SetActive(false);
   }
 }
