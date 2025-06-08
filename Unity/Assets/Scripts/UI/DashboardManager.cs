5bae3545d159502f86d8be118c7d0b41d1e765a9
MilanThenerd <milankruger4@gmail.com>
2025-06-08 12:15:39 +0200

    Added Analysis Page Routing

--- Unity/Assets/Scripts/UI/DashboardManager.cs
+++ Unity/Assets/Scripts/UI/DashboardManager.cs
@@ -6,16 +6,27 @@ public class DashboardManager : MonoBehaviour
 {
   public GameObject dashboard;
   public GameObject importTrack;
-    
+
+  public GameObject analysis;
+
   public void showDashboard()
   {
     dashboard.SetActive(true);
     importTrack.SetActive(false);
+    analysis.SetActive(false);
   }
 
   public void showImportTrack()
   {
     dashboard.SetActive(false);
     importTrack.SetActive(true);
+    analysis.SetActive(false);
+  }
+
+  public void showAnalysis()
+  {
+    dashboard.SetActive(false);
+    importTrack.SetActive(false);
+    analysis.SetActive(true);
   }
 }
