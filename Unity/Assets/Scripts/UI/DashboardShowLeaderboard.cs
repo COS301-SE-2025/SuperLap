463066a9e3894c4dce022587c2e3ecbf0a6348f2
MilanThenerd <milankruger4@gmail.com>
2025-06-15 15:19:55 +0200

    Added curved buttons to main dashboard

--- Unity/Assets/Scripts/UI/DashboardShowLeaderboard.cs
+++ Unity/Assets/Scripts/UI/DashboardShowLeaderboard.cs
@@ -1,6 +1,9 @@
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using UnityEngine.EventSystems;
+using System.Collections;
+using System.Collections.Generic;
 
 public class DashboardShowLeaderboard : MonoBehaviour
 {
@@ -10,12 +13,16 @@ public class DashboardShowLeaderboard : MonoBehaviour
   private float leaderboardWidth;
 
   public TMP_Text leaderboardButtonText;
+  public Button leaderboardButton;
 
   private Vector3 newPosition;
 
   public float transitionSpeed = 1000f;
-  
+
   private bool showLeaderboard = false;
+
+  public Vector3 HiddenPosition;
+  public Vector3 VisiblePosition;
 
   public void Start()
   {
@@ -23,39 +30,67 @@ public class DashboardShowLeaderboard : MonoBehaviour
     leaderboardWidth = rectTransform.rect.width;
     Debug.Log("Leaderboard Width: " + leaderboardWidth);
     showLeaderboard = false;
-    newPosition = new Vector3(-360 - leaderboardWidth, rectTransform.anchoredPosition.y, rectTransform.transform.position.z);
+    newPosition = HiddenPosition;
     rectTransform.anchoredPosition = newPosition;
   }
 
   void Update()
   {
-    if(rectTransform.anchoredPosition.x != newPosition.x)
+    if (rectTransform.anchoredPosition.x != newPosition.x)
     {
       rectTransform.anchoredPosition = Vector3.MoveTowards(rectTransform.anchoredPosition, newPosition, transitionSpeed * Time.deltaTime);
+      if (Mathf.Abs(rectTransform.anchoredPosition.x - newPosition.x) <= 0.01f)
+      {
+        rectTransform.anchoredPosition = newPosition;
+        RefreshUIUnderPointer();
+      }
     }
   }
 
   public void HideLeaderboard()
   {
     showLeaderboard = false;
-    newPosition = new Vector3(-360 - leaderboardWidth, rectTransform.anchoredPosition.y, 0);
-    rectTransform.anchoredPosition = new Vector3(-360, rectTransform.anchoredPosition.y, 0);
+    newPosition = HiddenPosition;
+    rectTransform.anchoredPosition = newPosition;
     leaderboardButtonText.text = ">";
   }
 
   public void transitionLeaderboard()
   {
     showLeaderboard = !showLeaderboard;
-    if(showLeaderboard)
+    if (showLeaderboard)
     {
-      newPosition = new Vector3(-360, rectTransform.anchoredPosition.y, 0);
+      newPosition = VisiblePosition;
       leaderboardButtonText.text = "<";
     }
     else
     {
-      newPosition = new Vector3(-360-leaderboardWidth, rectTransform.anchoredPosition.y, 0);
+      newPosition = HiddenPosition;
       leaderboardButtonText.text = ">";
     }
   }
-  
+
+  private void RefreshUIUnderPointer()
+  {
+    EventSystem eventSystem = EventSystem.current;
+
+    // Clear current selection and hover
+    eventSystem.SetSelectedGameObject(null);
+
+    PointerEventData pointerData = new PointerEventData(eventSystem)
+    {
+      position = Input.mousePosition
+    };
+
+    // Raycast to find what's under the pointer
+    var raycastResults = new List<RaycastResult>();
+    eventSystem.RaycastAll(pointerData, raycastResults);
+
+    // Tell Unity to re-hover the top result, if any
+    foreach (var result in raycastResults)
+    {
+      ExecuteEvents.Execute(result.gameObject, pointerData, ExecuteEvents.pointerExitHandler);
+      ExecuteEvents.Execute(result.gameObject, pointerData, ExecuteEvents.pointerEnterHandler);
+    }
+  }
 }
