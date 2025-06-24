fefeb569b6de425c389c0a18c854c52c2cd04024
Amber21457752 <u21457752@tuks.co.za>
2025-06-24 18:40:39 +0200

    Fixed the popup message error that was affecting the website's form submission

--- Website/js/script.js
+++ Website/js/script.js
@@ -1,7 +1,7 @@
 //------------------------------------------------------------------------- Download Button Functionality
 function download() {
     // Placeholder download action
-    alert("Download unavailable at the Moment. But we're working on it!");
+    alert("❌ Download unavailable at the Moment. But we're working on it!");
     // You can also trigger an actual file download like this:
     // window.location.href = 'path/to/your/file.pdf';
 }
@@ -65,7 +65,7 @@ leftButton.addEventListener('click', () => {
     container.classList.remove('right-open'); // Close right panel
 });
 
-//------------------------------------------------------------------------- Sent Eamil
+//------------------------------------------------------------------------- Send Eamil
 
 const form = document.getElementById("contact_form");
 const popup = document.getElementById("popup");
@@ -102,6 +102,8 @@ function showPopup(message) {
         popup.style.display = "none";
     }, 4000);
 }
+
+//------------------------------------------------------------------------- Slider code
 
 // Right button (starts the slider)
 document.querySelector('.right-button').addEventListener('click', function () {
