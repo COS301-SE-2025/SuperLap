2686e42ce830a213f64c5948e2235da199a411de
Amber21457752 <u21457752@tuks.co.za>
2025-08-10 22:27:54 +0200

    unet_block function added to TrackCNN.py for the main section of the database before the model is trained.

--- Backend/ImageProcessing/TrackCNN.py
+++ Backend/ImageProcessing/TrackCNN.py
@@ -87,3 +87,50 @@ def conv2d_block(inputTensor, numFilters, kernelSize=3, doBatchNorm=True):
     
     return x
 
+def unet_block(inputImage, numFilters=16, droupouts=0.1, doBatchNorm=True):
+    """
+    Creates a U-Net model for semantic segmentation.
+    """
+    # Encoder path
+    c1 = conv2d_block(inputImage, numFilters * 1, kernelSize=3, doBatchNorm=doBatchNorm)
+    p1 = tf.keras.layers.MaxPooling2D((2, 2))(c1)
+    p1 = tf.keras.layers.Dropout(droupouts)(p1)
+    
+    c2 = conv2d_block(p1, numFilters * 2, kernelSize=3, doBatchNorm=doBatchNorm)
+    p2 = tf.keras.layers.MaxPooling2D((2, 2))(c2)
+    p2 = tf.keras.layers.Dropout(droupouts)(p2)
+    
+    c3 = conv2d_block(p2, numFilters * 4, kernelSize=3, doBatchNorm=doBatchNorm)
+    p3 = tf.keras.layers.MaxPooling2D((2, 2))(c3)
+    p3 = tf.keras.layers.Dropout(droupouts)(p3)
+    
+    c4 = conv2d_block(p3, numFilters * 8, kernelSize=3, doBatchNorm=doBatchNorm)
+    p4 = tf.keras.layers.MaxPooling2D((2, 2))(c4)
+    p4 = tf.keras.layers.Dropout(droupouts)(p4)
+    
+    c5 = conv2d_block(p4, numFilters * 16, kernelSize=3, doBatchNorm=doBatchNorm)
+    
+    # Decoder path
+    u6 = tf.keras.layers.Conv2DTranspose(numFilters*8, (3, 3), strides=(2, 2), padding='same')(c5)
+    u6 = tf.keras.layers.concatenate([u6, c4])
+    u6 = tf.keras.layers.Dropout(droupouts)(u6)
+    c6 = conv2d_block(u6, numFilters * 8, kernelSize=3, doBatchNorm=doBatchNorm)
+    
+    u7 = tf.keras.layers.Conv2DTranspose(numFilters*4, (3, 3), strides=(2, 2), padding='same')(c6)
+    u7 = tf.keras.layers.concatenate([u7, c3])
+    u7 = tf.keras.layers.Dropout(droupouts)(u7)
+    c7 = conv2d_block(u7, numFilters * 4, kernelSize=3, doBatchNorm=doBatchNorm)
+    
+    u8 = tf.keras.layers.Conv2DTranspose(numFilters*2, (3, 3), strides=(2, 2), padding='same')(c7)
+    u8 = tf.keras.layers.concatenate([u8, c2])
+    u8 = tf.keras.layers.Dropout(droupouts)(u8)
+    c8 = conv2d_block(u8, numFilters * 2, kernelSize=3, doBatchNorm=doBatchNorm)
+    
+    u9 = tf.keras.layers.Conv2DTranspose(numFilters*1, (3, 3), strides=(2, 2), padding='same')(c8)
+    u9 = tf.keras.layers.concatenate([u9, c1])
+    u9 = tf.keras.layers.Dropout(droupouts)(u9)
+    c9 = conv2d_block(u9, numFilters * 1, kernelSize=3, doBatchNorm=doBatchNorm)
+    
+    output = tf.keras.layers.Conv2D(1, (1, 1), activation='sigmoid')(c9)
+    model = tf.keras.Model(inputs=[inputImage], outputs=[output])
+    return model
