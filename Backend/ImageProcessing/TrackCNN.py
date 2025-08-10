ec76acaaa719c07146b50e3364c70cab7bfc7c2d
Amber21457752 <u21457752@tuks.co.za>
2025-08-10 22:25:49 +0200

    save_model_summary function added to TrackCNN.py for saving the summary file of the model.

--- Backend/ImageProcessing/TrackCNN.py
+++ Backend/ImageProcessing/TrackCNN.py
@@ -9,3 +9,11 @@ from PIL import Image
 # Create output directory if it doesn't exist
 output_dir = "CNNoutput"
 os.makedirs(output_dir, exist_ok=True)
+
+def save_model_summary(model, filename):
+    """Save model summary to a text file"""
+    original_stdout = sys.stdout  # Save a reference to the original standard output
+    with open(filename, 'w') as f:
+        sys.stdout = f  # Change the standard output to the file we created
+        model.summary()
+        sys.stdout = original_stdout  # Reset the standard output to its original value
